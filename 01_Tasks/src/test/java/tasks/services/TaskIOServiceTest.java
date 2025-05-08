52c8e05463d177e90eb69f5a637bfa6cee585fce
Maria Raicu <mraicu0502@gmail.com>
2025-05-08 12:58:12 +0300

    BBT

--- 01_Tasks/src/test/java/tasks/services/TaskIOServiceTest.java
+++ 01_Tasks/src/test/java/tasks/services/TaskIOServiceTest.java
@@ -11,13 +11,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class TaskIOServiceTest {
     private Task task;
 
-    @BeforeAll
-    static void BeforeAll() {
-        TaskIOService.setTaskList(new ArrayList<>());
-    }
-
     @Test
-    void TC1_ECP() {
+    void addTaskValidECP() {
+        TaskIOService.setTaskList(new ArrayList<>());
 
         try {
             task = new Task("Task1",
@@ -38,51 +34,20 @@ class TaskIOServiceTest {
             e.printStackTrace();
         }
 
-        assertEquals("Task1", lastTask.getTitle());
-        assertEquals("2025-04-03 10:00", lastTask.getFormattedDateStart());
-        assertEquals("2025-04-06 20:05", lastTask.getFormattedDateEnd());
-        assertEquals(5, lastTask.getRepeatInterval());
-        assertTrue(lastTask.isActive());
-
-    }
-
-
-    @Test
-    void TC2_ECP() {
-        try {
-            task = new Task("Task3",
-                    Task.getDateFormat().parse("data inceput"),
-                    Task.getDateFormat().parse("2025-04-10 13:05"),
-                    7
-            );
-            task.setActive(false);
-            TaskIOService.add(task);
-        } catch (Exception e) {
-            e.printStackTrace();
-            assertEquals("Unparseable date: \"data inceput\"", e.getMessage());
+        if (lastTask != null) {
+            assertEquals("Task1", lastTask.getTitle());
+            assertEquals("2025-04-03 10:00", lastTask.getFormattedDateStart());
+            assertEquals("2025-04-06 20:05", lastTask.getFormattedDateEnd());
+            assertEquals(5, lastTask.getRepeatInterval());
+            assertTrue(lastTask.isActive());
         }
     }
 
     @Test
-    void TC3_ECP() {
+    void addTaskInvalidECP() {
+        TaskIOService.setTaskList(new ArrayList<>());
 
-        try {
-            task = new Task("Task4",
-                    Task.getDateFormat().parse("2025-04-15 10:00"),
-                    Task.getDateFormat().parse("2025-04-20 20:05"),
-                    -7
-            );
-            task.setActive(false);
-            TaskIOService.add(task);
-        } catch (Exception e) {
-            e.printStackTrace();
-            assertEquals("interval should be > 0", e.getMessage());
-        }
-    }
-
-    @Test
-    void TC1_BVA() {
-        try {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             task = new Task("",
                     Task.getDateFormat().parse("2025-04-07 05:00"),
                     Task.getDateFormat().parse("2025-04-10 12:23"),
@@ -90,84 +55,122 @@ class TaskIOServiceTest {
             );
             task.setActive(false);
             TaskIOService.add(task);
-        } catch (Exception e) {
-            assertEquals("Title cannot be empty", e.getMessage());
+        });
 
-            e.printStackTrace();
-        }
+        String expectedMessage = "Title cannot be empty";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+
+
+        exception = assertThrows(IllegalArgumentException.class, () -> {
+            task = new Task("Task2",
+                    Task.getDateFormat().parse("2025-04-15 07:00"),
+                    Task.getDateFormat().parse("2025-04-20 13:05"),
+                    -6
+            );
+            task.setActive(false);
+            TaskIOService.add(task);
+        });
+
+        expectedMessage = "interval should be > 0";
+        actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 
     @Test
-    void TC12_BVA() {
+    void addTaskValidBVA() {
+        TaskIOService.setTaskList(new ArrayList<>());
+
         try {
-            task = new Task("Task4",
-                    Task.getDateFormat().parse("2025-04-15 10:00"),
-                    Task.getDateFormat().parse("2034-04-20 20:05"),
-                    -7
+            task = new Task("Task1",
+                    Task.getDateFormat().parse("2025-04-03 10:00"),
+                    Task.getDateFormat().parse("2025-04-06 20:05"),
+                    5
+            );
+            task.setActive(true);
+            TaskIOService.add(task);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        Task lastTask = null;
+        try {
+            lastTask = TaskIOService.getLast();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (lastTask != null) {
+            assertEquals("Task1", lastTask.getTitle());
+            assertEquals("2025-04-03 10:00", lastTask.getFormattedDateStart());
+            assertEquals("2025-04-06 20:05", lastTask.getFormattedDateEnd());
+            assertEquals(5, lastTask.getRepeatInterval());
+            assertTrue(lastTask.isActive());
+        }
+
+        try {
+            task = new Task("Task6",
+                    Task.getDateFormat().parse("2050-04-15 10:00"),
+                    Task.getDateFormat().parse("2025-04-20 20:05"),
+                    3
             );
             task.setActive(false);
             TaskIOService.add(task);
         } catch (Exception e) {
-            assertEquals("interval should be > 0", e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    void TC8_BVA() {
-
-        try {
-            task = new Task("Task5",
-                    Task.getDateFormat().parse("2025-04-15 10:00"),
-                    Task.getDateFormat().parse("1950-04-20 20:05"),
-                    10
-            );
-            task.setActive(true);
-            TaskIOService.add(task);
-        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        Task lastTask = null;
         try {
             lastTask = TaskIOService.getLast();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        assertEquals("Task5", lastTask.getTitle());
-        assertEquals("2025-04-15 10:00", lastTask.getFormattedDateStart());
-        assertEquals("1950-04-20 20:05", lastTask.getFormattedDateEnd());
-        assertEquals(10, lastTask.getRepeatInterval());
-        assertTrue(lastTask.isActive());
-
+        if (lastTask != null) {
+            assertEquals("Task6", lastTask.getTitle());
+            assertEquals("2050-04-15 10:00", lastTask.getFormattedDateStart());
+            assertEquals("2025-04-20 20:05", lastTask.getFormattedDateEnd());
+            assertEquals(3, lastTask.getRepeatInterval());
+            assertFalse(lastTask.isActive());
+        }
     }
 
-
     @Test
-    void TC16_BVA() {
-        try {
-            task = new Task("Task7",
-                    Task.getDateFormat().parse("2025-04-15 10:00"),
-                    Task.getDateFormat().parse("2025-04-20 20:05"),
-                    1
-            );
-            task.setActive(true);
-            TaskIOService.add(task);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Task lastTask = null;
-        try {
-            lastTask = TaskIOService.getLast();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    void addTaskInvalidBVA() {
+        TaskIOService.setTaskList(new ArrayList<>());
 
-        assertEquals("Task7", lastTask.getTitle());
-        assertEquals("2025-04-15 10:00", lastTask.getFormattedDateStart());
-        assertEquals("2025-04-20 20:05", lastTask.getFormattedDateEnd());
-        assertEquals(1, lastTask.getRepeatInterval());
-        assertTrue(lastTask.isActive());
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            task = new Task("Task2",
+                    Task.getDateFormat().parse("2051-04-15 07:00"),
+                    Task.getDateFormat().parse("2025-04-20 13:05"),
+                    2
+            );
+            task.setActive(false);
+            TaskIOService.add(task);
+        });
+
+        String expectedMessage = "Start date cannot be > 2050";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+
+
+        exception = assertThrows(IllegalArgumentException.class, () -> {
+            task = new Task("Task3",
+                    Task.getDateFormat().parse("1979-04-10 13:05"),
+                    Task.getDateFormat().parse("2025-04-10 13:05"),
+                    7
+            );
+            task.setActive(false);
+            TaskIOService.add(task);
+        });
+
+        expectedMessage = "Start date cannot be < 1980";
+        actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 }
